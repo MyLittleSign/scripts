83dcafadc4150cbedbc6979ed1e9c425b4cdf736
tkovs <victor.rdg@hotmail.com>
2017-02-14 01:34:00 -0300

    Thinking about the database

---
+++ Database.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Database : MonoBehaviour {
+	/* Cada tela é uma classe composta por n letras e 1 exercício.
+	 *   - Cada letra tem um conjunto de lições visando o aprendizado de n palavras.
+	 *       - Uma letra tem 1 som, 1 gesto e um símbolo.
+	 *       - Cada palavra tem 1 som, 1 gesto e uma figura.
+	 *   - Cada exercício tem 4 tarefas para que o usuário faça a assimilação do aprendizado, e 3 estrelas para avaliar
+	 *     o usuário.
+	 *       - 1º tarefa:
+	 *       - 2º tarefa:
+	 *       - 3º tarefa:
+	 *       - 4º tarefa:
+	 */
+}
