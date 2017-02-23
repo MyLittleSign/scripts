3bdb15f116bc52a5a2dac371119b77c4b3b99851
tkovs <victor.rdg@hotmail.com>
2017-02-23 17:36:56 -0300

    Script para reset do banco de dados; Alterações LetterButton

---
+++ DeleteKeys.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteKeys : MonoBehaviour {
+
+	void Start ()
+	{
+		PlayerPrefs.DeleteAll ();
+	}
+}
--- LetterButton.cs
+++ LetterButton.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 public class LetterButton : MonoBehaviour
 {
-	/* Cada botão que leva a uma cena de aprendizado de letras tem uma tag contendo a letra dele.
-	 * Exemplo de tag: "g"	 * 
-	 * O atual script será anexado a um botão, e através de sua tag, reconhecerá a letra com a qual deve trabalhar.
+	/* Cada botão que leva a uma cena de aprendizado de letras tem uma name contendo a letra dele.
+	 * Exemplo de name: "Learn_g"
+	 * O atual script será anexado a um botão, e através de sua name, reconhecerá a letra com a qual deve trabalhar.
 	 * Após reconhecer a letra, obterá todas as informações necessárias ao botão:
 	 *   - Disponível ou não ao usuário.
 	 * Após descobrir se está disponível ou não, este script altera a imagem do botão para a disponibilidade ficar
@@ -19,20 +20,49 @@ public class LetterButton : MonoBehaviour
 	// enabled informará se a letra atual está disponível para aprendizado ou não
 	private string s_enabled;
 	private int enabled;
+	private string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 	private void Awake ()
 	{
-		letter = gameObject.tag;
+		string name = gameObject.name;
+		letter = name.Substring (name.Length - 1);
 
 		s_enabled = String.Concat ("enabled_", letter);
 
 		if (!PlayerPrefs.HasKey (s_enabled))
 		{
-			PlayerPrefs.SetInt (s_enabled, 0);
+			if (letter == "a")
+			{
+				PlayerPrefs.SetInt (s_enabled, 1);
+			}
+			else
+			{
+				PlayerPrefs.SetInt (s_enabled, 0);
+			}
 		}
 		else
 		{
 			enabled = PlayerPrefs.GetInt (s_enabled);
+
+			if (enabled == 1)
+			{
+				this.GetComponent <Button> ().interactable = true;
+			}
+			else
+			{
+				this.GetComponent <Button> ().interactable = false;
+			}
+		}
+	}
+
+	public void Done ()
+	{
+		if (letter != "z")
+		{
+			string next_letter = alphabet [alphabet.IndexOf (letter) + 1].ToString ();
+			string new_enabled = String.Concat ("enabled_", next_letter);
+
+			PlayerPrefs.SetInt (new_enabled, 1);
 		}
 	}
 }
